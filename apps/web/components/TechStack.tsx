5d1e665aede9b04937e30db359480a3e9b6e13bd
Jimmy-02 <tainat1202@gmail.com>
2026-07-06 15:34:47 +0700

    chore: remove unused import

--- apps/web/components/TechStack.tsx
+++ apps/web/components/TechStack.tsx
@@ -19,7 +19,6 @@ import {
   SiExpress,
   SiNestjs,
   SiGithubactions,
-  SiScrumalliance,
 } from "react-icons/si";
 import { LuFolderTree } from "react-icons/lu";
 import Container from "@/components/Container";
